bf6a0ba5176c6291208b2a25a0ae987554ac292a
zz <zz>
2023-08-01 15:39:36 +0800

    s

--- wildcards/test.py
+++ wildcards/test.py
@@ -2,11 +2,11 @@ import os
 import re
 
 str = ''
-for root, ds, fs in os.walk(f'./pose'):
+for root, ds, fs in os.walk(f'./clothes'):
     for f in fs:
         fullname = os.path.join(root, f)
         with open(fullname, 'r') as f:
             str+= f'{f.read()}\n'
 
-with open('./pose.txt', 'w', encoding='utf-8') as f:
+with open('./clothes.txt', 'w', encoding='utf-8') as f:
     f.write(str)
