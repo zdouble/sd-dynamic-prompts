aa1f0c2f17bb9a492de3258e150a0e7fb1bca773
zdouble <you@example.com>
2024-10-02 19:30:34 +0800

    s

--- wildcards/test.py
+++ wildcards/test.py
@@ -2,11 +2,11 @@ import os
 import re
 
 str = ''
-for root, ds, fs in os.walk(f'./bg/science_fiction'):
+for root, ds, fs in os.walk(f'./animalsAutoprompt_v15'):
     for f in fs:
         fullname = os.path.join(root, f)
         with open(fullname, 'r') as f:
             str+= f'{f.read()}\n'
 
-with open('./bg.txt', 'w', encoding='utf-8') as f:
+with open('./animals.txt', 'w', encoding='utf-8') as f:
     f.write(str)
